44d17673704bcc077763c87a04d69bd3f8d709c8
Marceline Ziegler <manicpixiegirl13@gmail.com>
2018-04-28 16:58:24 -0400

    Eddited unit6

--- unit6.py
+++ unit6.py
@@ -1,2 +1,11 @@
 excercise = "Excercise 6!"
 print(excercise)
+
+myfile = open('/home/hypatian/hypatia-excercises/inputfile.txt')
+file_contents = myfile.read()
+myfile.close()
+print(file_contents)
+
+outputfile = open('outputfile.txt', "w")
+outputfile.write(file_contents)
+outputfile.close()
